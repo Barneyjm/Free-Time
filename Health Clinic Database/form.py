ed17617418abfff3d84da0d5164bfa42fb9a01cc
James Barney <jamesbarney71>
2013-11-08 00:16:51 -0500

    Create Health Clinic Database

---
+++ Health Clinic Database/form.py
@@ -0,0 +1,30 @@
+from Tkinter import *
+import tkMessageBox
+import tkFileDialog
+import time, os
+
+#
+# Health Clinic Database -- creates and stores personal health records 
+#
+# @author James Barney
+# @date 2013-11-8
+# @version 1.0
+#
+
+class EntryForm:
+    """
+    Creates a form for the user to input data about the patient.
+    --Creates a new patient record
+    --Injury type
+    --Treatment type
+    """
+    def __init__():
+        pass
+        
+class LoginForm:
+    """
+    Ensures that the database is running and then logs in, provided correct credentials.
+    """
+    def __init__():
+        pass
+        
---
+++ Health Clinic Database/geany_run_script.bat
@@ -0,0 +1,6 @@
+C:\Python27\python "form.py"
+
+pause
+del "%0"
+
+pause
