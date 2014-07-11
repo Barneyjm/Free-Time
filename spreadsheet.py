600cf760e86660420055afac4551396f31f9dfb7
James Barney <Jamesbarney71@gmail.com>
2014-07-11 15:45:16 -0400

    woo functionality!

--- spreadsheet.py
+++ spreadsheet.py
@@ -4,25 +4,51 @@ Created on Wed Jul  9 15:59:04 2014
 
 @author: james
 """
+from pprint import pprint
 
 class Spreadsheet(object):
-    def __init__(self, x=50, y=50):
-        self.sheet = [[Cell(i,i) for i in xrange(x)] for i in xrange(y)]
-    
+    def __init__(self, x=3, y=3):
+        self.sheet_num = 0
+        self.sheets = {}
+        self.sheets[self.sheet_num] = self.make_sheet(x, y)
+
+    def make_sheet(self, x, y):
+        self.sheet_num += 1
+        return [[Cell(i,i) for i in xrange(x)] for i in xrange(y)]        
+
+    def get_row(self, x, sheet=1):
+        return self.sheets[sheet][x]
+
+    def get_col(self, y, sheet=1):
+        return [row[y] for row in self.sheets[sheet] ]
+
+    def get_cell_content(self, x, y, sheet=1):
+        return self.sheets[sheet][x][y].get_content()
+
+    def set_cell_content(self, x, y, content, sheet=1):
+        self.sheets[sheet][x][y].set_content(content)
+
+    def mult_cells(self, x1, y1, x2, y2, sheet=1):
+        return self.get_cell_content(x1,y1)*self.get_cell_content(x2,y2)
+        
+        
 class Cell(object):
     def __init__(self, xid, yid):
-        self._contents = None
+        self._content = None
         self.ID = str(xid) + str(yid)
         
         self._height = 10.0
         self._width = 40.0
         self.neighbors = {"top": None, "bottom": None, "left": None, "right": None}
+
+    def __repr__(self):
+        return str(self.get_content())
         
-    def set_contents(self, content):
-        self._contents = content
+    def set_content(self, content):
+        self._content = content
         
-    def get_contents(self):
-        return self._contents
+    def get_content(self):
+        return self._content
         
     def set_height(self, height):
         self.height = height
@@ -38,4 +64,12 @@ class Cell(object):
         
 if __name__ == '__main__':
     sheet = Spreadsheet()
+
+    pprint(sheet.sheets[1])
+    sheet.set_cell_content(0,0, 5)
+    sheet.set_cell_content(0,1, 3)
+    pprint(sheet.sheets[1])
     
+    print sheet.mult_cells(0,0,0,1)
+    
+   
