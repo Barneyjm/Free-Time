038a763020311f4f658de42fed4ad809c1e90118
James Barney <Jamesbarney71@gmail.com>
2014-10-02 14:20:36 -0400

    types.py

---
+++ UpDown/types.py
@@ -0,0 +1,35 @@
+#types of elevators, cables, buildings
+
+
+######### elevators ##########
+# self, cur_floor, cur_weight, cur_speed, cur_accl,
+#         weight_limit, speed_limit, cable_type)
+
+
+E_basic = Elevator(0, 500, 0, 0, 3000, 20, "dunno")
+E_medium = Elevator(0, 750, 0, 0, 5000, 30, "dunno")
+E_advanced = Elevator(0, 1000, 0, 0, 10000, 40, "dunno")
+
+
+######### cables #############
+#(self, safety_rating, max_tension, mass_per_len)
+
+C_basic = Cable(3, )
+C_medium = Cable()
+C_advanced = Cable()
+
+######### buildings ##########
+#(self, num_floors, floor_cap, num_elevators)
+
+B_basic = Building()
+B_medium = Building()
+B_advanced = Building()
+
+
+
+######### persons ##########
+#(self, height, mass)
+
+P_small = Person()
+P_medium = Person()
+P_large = Person()
