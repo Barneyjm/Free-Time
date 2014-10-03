47680f9d5ed0f37f78219fd07fc55fa96264055d
James Barney <Jamesbarney71@gmail.com>
2014-10-03 13:57:38 -0400

    Update main.py

--- UpDown/main.py
+++ UpDown/main.py
@@ -1,22 +1,38 @@
-import types
+from models import Models as models
+from objects import *
 import elevatorTitle
+
+import math
 
 
 class UpDown(object):
     def __init__(self, player_name, difficulty):
         self.player_name = player_name
         self.difficulty = difficulty
+        self.score = 0
 
-    def start(self):
-        #print elevatorTitle.title
+    def start(self, player, difficulty, score):
 
-        print "welcome " + self.player_name
-        print "you're playing on difficulty: " + str(self.difficulty)
+        print "welcome " + player
+        print "you're playing on difficulty: " + str(difficulty)
+        print "you have a score of: " + str(score)
 
+        #elevator = models.E_basic
+        #print elevator.cur_weight
+
+        self.generate_initial_objects()
+
+        self.main_loop(player, difficulty, score)
+
+
+    def main_loop(player, difficulty, score):
+        pass
+
+    def generate_initial_objects(self):
+        building = models.B_basic
+        elevator = models.E_basic
+        all_people = []
         
-        elevator = types.E_basic
-        print elevator
-
 
 
 
@@ -28,4 +44,4 @@ class UpDown(object):
 
 if __name__ == "__main__":
     game = UpDown("James", 1)
-    game.start()
+    game.start("James", 1, 0)
